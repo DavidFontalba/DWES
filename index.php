a0e62b509c13298c3ba6d1e699ddf502336a878b
David Fontalba <davidfontalba98@gmail.com>
2020-10-27 10:13:20 +0100

    Añadido index

---
+++ index.php
@@ -0,0 +1,189 @@
+<?php
+$ejercicios = array(
+  "Ejercicios práctica 7, funciones" =>
+  array(
+    "Ej 5: Lista de enlaces" => "index.php",
+    "Ej 4: Suma dígitos" => "ejercicios/p7funciones/ej4sumadigitos.php",
+    "Ej 3: Genera nombre de usuario" => "ejercicios/p7funciones/ej3username.php",
+    "Ej 2: Factoriza" => "ejercicios/p7funciones/ej2factoriza.php",
+    "Ej 1: Genera letra DNI" => "ejercicios/p7funciones/ej1dni.php",
+    "Prueba 2, arrayMap" => "ejercicios/p7funciones/pruebasFunc/arrayMap.php",
+    "Prueba 1, variables estáticas" => "ejercicios/p7funciones/pruebasFunc/estaticas.php"
+
+  ),
+
+  "Ejercicios práctica 6, formularios" =>
+  array(
+    "Ej 5: Test de verbos irregulares" => "ejercicios/p6formularios/ej5testirregulares.php",
+    "Prueba 2, formulario php" => "ejercicios/p6formularios/pruebaConFormularios/formulario2.php",
+    "Prueba 1, formulario html" => "ejercicios/p6formularios/pruebaConFormularios/formulario1.html"
+  ),
+
+  "Ejercicios práctica 5, arrays" =>
+  array(
+    "Ej 4: Carta de un restaurante" => "ejercicios/p5arrays/ej4carta.php",
+    "Ej 3: Array con información" => "ejercicios/p5arrays/ej3arrayInfo.php",
+    "Ej 2: Alumnos aleatorios" => "ejercicios/p5arrays/ej2alumnoAleatorio.php",
+    "Prueba 3" => "ejercicios/p5arrays/pruebasConProvincias/prueba3.php",
+    "Prueba 2" => "ejercicios/p5arrays/pruebasConProvincias/prueba2.php",
+    "Prueba 1" => "ejercicios/p5arrays/pruebasConProvincias/prueba.php"
+  ),
+
+  "Ejercicios práctica 4, bucles" =>
+  array(
+    "Ej 5: Calendario" => "ejercicios/p4bucles/ej5calendario.php",
+    "Ej 4: Paleta de colores" => "ejercicios/p4bucles/ej4colores.php",
+    "Ej 3: Tablas de multiplicar" => "ejercicios/p4bucles/ej3tablamul.php",
+    "Ej 2: Suma los 3 primeros pares" => "ejercicios/p4bucles/ej2pares.php",
+    "Ej 1: Números 1 a 10" => "ejercicios/p4bucles/ej1nums.php"
+  ),
+
+  "Ejercicios práctica 3, condicionales" =>
+  array(
+    "Ej 5: Distintos enlaces según usuario" => "ejercicios/p3if/ej5usuarios.php",
+    "Ej 4: Estaciones" => "ejercicios/p3if/ej4cabeceraEstaciones.php",
+    "Ej 3: Calcular edad" => "ejercicios/p3if/ej3calcularEdad.php",
+    "Ej 2: Días según mes y año" => "ejercicios/p3if/ej2diasDelMes.php",
+    "Ej 1: El mayor de dos números" => "ejercicios/p3if/ej1numMayor.php"
+  ),
+
+  "Ejercicios práctica 2, basicos" =>
+  array(
+    "Ej 7: Tipos" => "ejercicios/p2/ej7tipos.php",
+    "Ej 6: Harry" => "ejercicios/p2/ej6Harry.php",
+    "Ej 5: Valor actual" => "ejercicios/p2/ej5valoractual.php",
+    "Ej 4: Variables" => "ejercicios/p2/ej4variables.php",
+    "Ej 3: Suma" => "ejercicios/p2/ej3suma.php",
+    "Ej 2: Área círculo" => "ejercicios/p2/ej2areacirculo.php",
+    "Ej 1: Ficha Personal" => "ejercicios/p2/ej1fichapersonal.php"
+  )
+);
+
+function crearLinks($temas)
+{
+  foreach ($temas as $titulo => $ejercicios) {
+    echo "<section><h3>" . $titulo . "</h3>";
+
+    foreach ($ejercicios as $ejercicio => $ruta) {
+      echo '<p><a href="' . $ruta . '"target="_blank">' . $ejercicio . '</a></p>';
+    }
+    echo "</section>";
+  }
+}
+?>
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="author" content="David Galván Fontalba"/>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="css/style.css"/>
+
+  <title>David Galván Fontalba</title>
+</head>
+
+<body>
+  <div class="container">
+    <div id="resume-header" class="row">
+      <div class="col-3">
+        <img id="profile-picture" src="https://ui-avatars.com/api/?name=Dwes+Doe&size=255" alt="">
+      </div>
+      <div class="col">
+        <h1><?php echo "David Galván Fontalba"; ?></h1>
+        <h2><?php echo "DWES"; ?></h2>
+        <ul>
+          <li><?php echo "Mail: a19gafoda@iesgrancapitan.org"; ?></li>
+          <li><?php echo "Twitter: @DavidFontalba"; ?></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="border-bottom-gray">Carrer Summary</h2>
+        <p>
+
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div>
+          <h3 class="border-bottom-gray">Work Experience</h3>
+          <?php crearLinks($ejercicios); ?>
+        </div>
+        <div>
+          <h3 class="border-bottom-gray">Projects</h3>
+          <div class="project">
+            <h5>Project X</h5>
+            <div class="row">
+              <div class="col-3">
+                <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+              </div>
+              <div class="col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                <strong>Technologies used:</strong>
+                <span class="badge badge-secondary">PHP</span>
+                <span class="badge badge-secondary">HTML</span>
+                <span class="badge badge-secondary">CSS</span>
+              </div>
+            </div>
+          </div>
+          <div class="project">
+            <h5>Project X</h5>
+            <div class="row">
+              <div class="col-3">
+                <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+              </div>
+              <div class="col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                <strong>Technologies used:</strong>
+                <span class="badge badge-secondary">PHP</span>
+                <span class="badge badge-secondary">HTML</span>
+                <span class="badge badge-secondary">CSS</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <h3 class="border-bottom-gray">Skills & Tools</h3>
+        <h4>Backend</h4>
+        <ul>
+          <li>PHP</li>
+        </ul>
+        <h4>Frontend</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <h4>Frameworks</h4>
+        <ul>
+          <li>Bootstrap</li>
+        </ul>
+        <h3 class="border-bottom-gray">Languages</h3>
+        <ul>
+          <li>Spanish</li>
+          <li>English</li>
+        </ul>
+      </div>
+    </div>
+    <div id="resume-footer" class="row">
+      <div class="col">
+        Designed by @hectorbenitez
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+</body>
+
+</html>
