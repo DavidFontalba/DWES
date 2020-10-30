b7385267a81339136710a3783ab9de15344a98bc
David Fontalba <davidfontalba98@gmail.com>
2020-10-30 11:13:07 +0100

    Corregido DNI

--- ejercicios/p7funciones/ej1dni.php
+++ ejercicios/p7funciones/ej1dni.php
@@ -5,12 +5,13 @@ function clearData($cadena)
     return stripslashes(htmlspecialchars(trim($cadena)));
 }
 
-function procesaDNI($num) {
+function procesaDNI($num)
+{
     $letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
     if ($num <= 9999999 || $num > 99999999) {
         return false;
     } else {
-        return $num.$letras[$num%23];
+        return $num . $letras[$num % 23];
     }
 }
 ?>
@@ -28,16 +29,17 @@ function procesaDNI($num) {
 <body>
     <section>
         <?php
-        if (!isset($_GET['done'])) {
-            echo '<form href="./ej1dni.php" method="GET">';
-            echo '<input type="text" name="num" placeholder="Introduce el número de tu DNI"/>';
-            echo '<input type="submit" name="done" value="Enviar"/></form>';
-        } else {
+        echo '<form href="./ej1dni.php" method="GET">';
+        echo '<input type="text" name="num" placeholder="Introduce el número de tu DNI"/>';
+        echo '<input type="submit" name="done" value="Enviar"/></form>';
+        if (isset($_GET['done'])) {
             $dni = procesaDNI($_GET['num']);
             if ($dni) {
-                echo '<p>Tu DNI es: '.$dni.'</p>';
+                echo '<p>Tu DNI es: ' . $dni . '</p>';
+            } else if (empty($_GET['num'])) {
+                echo '<p>El DNI está vacío</p>';
             } else {
-                echo '<p>El DNI con número '.$_GET['num'].' no es válido';
+                echo '<p>El DNI con número ' . $_GET['num'] . ' no es válido';
             }
         }
         ?>
